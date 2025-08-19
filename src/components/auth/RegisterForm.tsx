ff61f934e5be5d073cd584785af9717ea9677f9a
Martin Weingart <martinweingart@gmail.com>
2025-08-19 10:01:48 -0300

    Add missing default value for name

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -42,6 +42,7 @@ export function RegisterForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
